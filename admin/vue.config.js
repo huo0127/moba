66c7e3855f0c324e99be3bcede5759763fad3bce
huo <reason0127301@gmail.com>
2021-11-09 23:21:19 +0800

    重構

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,6 +1,16 @@
 module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        assets: '@/assets',
+        common: '@/common',
+        components: '@/components',
+        network: '@/network',
+        views: '@/views',
+        store: '@store',
+      },
+    },
+  },
   outputDir: __dirname + '/../server/admin',
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/admin/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/admin/' : '/',
 }
